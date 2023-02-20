0307d985a79ca6d52fd328fae58cc08dad6dc05c
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2023-02-19 21:48:19 -0800

    added header and footer temporary

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,20 @@ import Skills from "@/components/brands/Skills";
 import About from "@/components/About";
 // import Portfolio from "@/components/work/Portfolio";
 import Contact from "@/components/Contact";
+import Footer from "@/components/navigation/Footer";
+import Header from "@/components/navigation/Header";
 
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
+      <Header />
       <Hero />
       <Skills />
       <About />
       {/* <Portfolio /> */}
       <Contact />
+      <Footer />
     </>
   );
 };
